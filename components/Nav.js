7a94f0e9b55d52628609261369f3b1955d139802
Aaron Morris <aaron@vercel.com>
2022-06-03 13:38:45 -0400

    :hammer: Add toasts page back so that we can see unread messages again

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import NotificationFeed from "./NotificationFeed";
-import NotificationToaster from "./NotificationToaster";
 import Avatar from "./Avatar";
 import styles from "../styles/Home.module.css";
 
@@ -11,11 +10,11 @@ export default function Nav() {
     <nav className={styles.nav}>
       <section>
         <Link href="/">Demo Knock App</Link>
+        <Link href="/toast">Toasts</Link>
         <Link href="/settings">Settings</Link>
       </section>
       <section>
         <NotificationFeed />
-        <NotificationToaster />
         <Avatar />
       </section>
     </nav>
