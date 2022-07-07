cf55f8b237ea2d58489ba8237dc70013a476afe0
Aaron Morris <aaron@vercel.com>
2022-07-07 15:30:21 -0400

    Add error boundary for notifications

--- components/Nav.js
+++ components/Nav.js
@@ -3,6 +3,32 @@ import NotificationFeed from "./NotificationFeed";
 import Avatar from "./Avatar";
 import styles from "../styles/Home.module.css";
 
+class NotificationErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>(Notification error)</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
 export default function Nav() {
   return (
     <nav className={styles.nav}>
@@ -12,7 +38,9 @@ export default function Nav() {
         <Link href="/settings">Settings</Link>
       </section>
       <section>
-        <NotificationFeed />
+        <NotificationErrorBoundary>
+          <NotificationFeed />
+        </NotificationErrorBoundary>
         <Avatar />
       </section>
     </nav>
